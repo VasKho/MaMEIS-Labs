fc3cac057304fd1e11eecf7c9f54f7dbef783b88
VasKho <vasya.khoroshavin@gmail.com>
2022-02-19 16:56:19 +0300

    Improve brute force algo

--- Lab_1/password_generator.py
+++ Lab_1/password_generator.py
@@ -1,40 +1,40 @@
 import random
+import itertools
 
 
 def generate_password(length: int, chars: str) -> str:
     password = ''
     for _ in range(length):
-        password = password + random.choice(list(chars))
+        password += random.choice(list(chars))
     return password
 
 
 def brute_force(length: int, chars: str, correct_password: str) -> None:
-    wrong_passwords = []
-    while True:
-        password = ''
-    
-        for _ in range(length):
-            password += random.choice(list(chars))
-     
-        if password not in wrong_passwords:
-            if password != correct_password:
-                wrong_passwords.append(password)
-            else:
-                print(password)
-                break
+    password = itertools.product(chars, repeat = length)
+    for i in password:
+        str = ''.join(i)
+        if str == correct_password:
+            print(str)
+            break
 
 
 if __name__ == '__main__':
     chars = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789'
-    length = input('Enter password length: ')
+    length = int(input('Enter password length: '))
 
     hitrate = dict.fromkeys(chars, 0)
 
     for i in range(10000):
-        user_password = generate_password(int(length), chars)
+        user_password = generate_password(length, chars)
         for elem in user_password:
             hitrate[elem] += 1
 
     for char in hitrate:
         if hitrate[char] > 0:
             print(char, hitrate[char])
+
+    passwd = generate_password(length, chars)
+    print(passwd)
+
+
+    brute_force(int(length), chars, passwd)
