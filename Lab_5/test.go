a68fa8d53c6d502898c03f8b9481e41b725ed038
VasKho <vasya.khoroshavin@gmail.com>
2022-05-06 10:56:48 +0300

    Add digital sign

--- Lab_5/test.go
+++ Lab_5/test.go
@@ -3,19 +3,18 @@ package main
 import utils "cryptutils/cryptutils"
 import "fmt"
 
-
 func main() {
-    a, b := utils.GenerateRSAKeys()
+	a, b := utils.GenerateRSAKeys()
 
-    // fmt.Println(a)
-    // fmt.Println(b)
+	var message string
+	fmt.Print("Enter message to encrypt: ")
+	fmt.Scanln(&message)
 
-    var message string
-    fmt.Print("Enter message to encrypt: ")
-    fmt.Scanln(&message)
+	enc := utils.EncryptRSA(message, a)
 
-    enc := utils.EncryptRSA(message, a)
+	fmt.Println("Encrypted", enc)
+	fmt.Println("Decrypted", utils.DecryptRSA(enc, b))
 
-    fmt.Println(enc)
-    fmt.Println(utils.DecryptRSA(enc, b))
+	signed := utils.Create_sign(message, b)
+	fmt.Println(utils.Check_sign(signed, a))
 }
